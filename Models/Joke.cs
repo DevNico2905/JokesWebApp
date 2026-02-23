14af6d4c7e997b83c65fb6a34a05c50c4c927b21
DevNico2905 <nicobernal2905@gmail.com>
2026-02-23 18:03:00 -0500

    first commit

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace JokesWebApp.Models;
+
+public class Joke
+{
+    public int Id { get; set; }
+    public string JokeQuestion { get; set; } = string.Empty;
+    public string JokeAnswer { get; set; } = string.Empty;
+
+    public Joke()
+    {
+        
+    }
+
+}
